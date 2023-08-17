e06eca34239ec70281c96f8ac4b250997462b170
Kuro <kuroguo@outlook.com>
2023-08-18 04:13:40 +0800

    Init

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const multer = require('multer')
+
+const app = express()
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+      callback(null, 'uploads')
+    },
+    filename: (req, file, callback) => {
+      callback(null, file.originalname)
+    }
+  })
+  
+  // 创建 Multer 实例
+  const upload = multer({ storage: storage });
+
+app.use(express.static('public'))
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  res.send('File uploaded')
+})
+
+app.listen(8080, () => {
+  console.log('Server started on port 8080')
+})
