83064940b5faaf0126e679f12e35c0d343d13b4f
Kuro <kuroguo@outlook.com>
2023-10-10 02:55:31 +0800

    Code indentation & removing a semicolon

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const multer = require('multer')
 const app = express()
 
 const storage = multer.diskStorage({
-    destination: (req, file, callback) => {
-      callback(null, 'uploads')
-    },
-    filename: (req, file, callback) => {
-      callback(null, Buffer.from(file.originalname, 'latin1').toString('utf8'))
-    }
-  })
+  destination: (req, file, callback) => {
+    callback(null, 'uploads')
+  },
+  filename: (req, file, callback) => {
+    callback(null, Buffer.from(file.originalname, 'latin1').toString('utf8'))
+  }
+})
   
-  // 创建 Multer 实例
-  const upload = multer({ storage: storage });
+// 创建 Multer 实例
+const upload = multer({ storage: storage })
 
 app.use(express.static('public'))
 
